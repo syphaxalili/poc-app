270bcab0e18d408b5134835e53107618804984f7
Syphax <sif7gmr@gmail.com>
2025-07-03 12:25:31 +0200

    Add registration logic

--- back/server.js
+++ back/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./src/config/db");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 const authRoutes = require("./src/routes/authRoutes");
 const fileRoutes = require("./src/routes/fileRoutes");
@@ -17,15 +19,15 @@ app.use("/api", huggingRoutes);
 
 // Connexion à la base de données et démarrage du serveur
 const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error("Failed to start server:", error.message);
-        process.exit(1);
-    }
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
 };
 
 startServer();
